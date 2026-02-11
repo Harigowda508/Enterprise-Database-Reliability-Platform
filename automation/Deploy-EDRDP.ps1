838dc02a745154ad94c9b693c507b4dfcb833cf4
Harish M <your-email@gmail.com>
2026-02-11 16:09:29 +0530

    changes done accoding for deployment

--- automation/Deploy-EDRDP.ps1
+++ automation/Deploy-EDRDP.ps1
@@ -2,10 +2,10 @@ $server = "localhost"
 $database = "EDRDP_DEV"
 
 # Step 1: Create database (run against master)
-sqlcmd -S $server -d master -i ".\database\CreateDatabase.sql"
+sqlcmd -S $server -d master -i ".\database\00_DB_creation\00_CreateDatabase.sql"
 
 # Step 2: Deploy remaining scripts
-$sqlFiles = Get-ChildItem ".\database\deployment\*.sql" | Sort-Object Name
+$sqlFiles = Get-ChildItem ".\database\01_deployment\*.sql" | Sort-Object Name
 
 foreach ($file in $sqlFiles) {
     Write-Host "Executing $($file.Name)"
